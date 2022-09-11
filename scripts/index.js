17f3e7eb985db01efa75e545e8cb25b9f169bd18
Lucas Novaes <lucasmsn200@gmail.com>
2022-09-10 21:36:58 -0300

    adding menu item expand functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const toggleMenuElement = document.getElementById('toggleMenu');
-
-    toggleMenuElement.addEventListener('click', () => {
-        const menuElement = document.getElementById('menu');
-        if (menuElement.classList.contains("hidden")) {
-            menuElement.classList.remove("hidden");
-        } else {
-            menuElement.classList.add("hidden");
-        }
-    })
-
-    const menuCloseElement = document.getElementById('menuClose');
-
-    menuCloseElement.addEventListener('click', () => {
-        const menuElement = document.getElementById('menu');
-
-        menuElement.classList.add("hidden");
-    })
-
-})
-
  
+    toggleMenuElement.addEventListener('click', () => {
+       const menuElement = document.getElementById('menu');
+       if (menuElement.classList.contains("hidden")) {
+          menuElement.classList.remove("hidden");
+       } else {
+          menuElement.classList.add("hidden");
+       }
+    })
+ 
+    const menuCloseElement = document.getElementById('menuClose');
+ 
+    menuCloseElement.addEventListener('click', () => {
+       const menuElement = document.getElementById('menu');
+ 
+       menuElement.classList.add("hidden");
+    })
+ 
+    const menuOptions = document.getElementsByClassName('menu-items__option');
+ 
+    for (const menuOption of menuOptions) {
+       const menuOptionIcon = menuOption.querySelector('i');
+       const menuOptionList = menuOption.nextElementSibling;
+ 
+       menuOption.addEventListener('click', () => {
+          if (menuOption.classList.contains('menu-items__option--active')) {
+             menuOption.classList.remove('menu-items__option--active')
+             menuOptionIcon.classList.remove('fa-minus');
+             menuOptionIcon.classList.add('fa-plus');
+             menuOptionList.classList.add('hidden')
+          } else {
+             menuOption.classList.add('menu-items__option--active')
+             menuOptionIcon.classList.remove('fa-plus');
+             menuOptionIcon.classList.add('fa-minus');
+             menuOptionList.classList.remove('hidden')
+          }
+       })
+    }
+ 
+ })
